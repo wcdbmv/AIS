6bd64e05013acef709f20e0567068a9caf9881c9
Kerimov Ahmed <kerimov.dev@yandex.ru>
2022-01-13 14:38:15 +0300

    Fix

--- src/4-genetic/main.py
+++ src/4-genetic/main.py
@@ -68,6 +68,7 @@ def mutation(bitstring, r_mut):
 
 # genetic algorithm
 def genetic_algorithm(objective, bounds, n_bits, n_iter, n_pop, r_cross, r_mut):
+    r_mut0 = r_mut
     # initial population of random bitstring
     pop = [randint(0, 2, n_bits * len(bounds)).tolist() for _ in range(n_pop)]
 
@@ -82,15 +83,33 @@ def genetic_algorithm(objective, bounds, n_bits, n_iter, n_pop, r_cross, r_mut):
         # evaluate all candidates in the population
         scores = [objective(d) for d in decoded]
 
+        a = [[scores[i], i] for i in range(len(scores))]
+        a.sort(key=lambda x: x[0])
+
+        best_parents = []
+        for i in range(10):
+            best_parents.append(pop[a[i][1]])
+
         # check for new best solution
         for i in range(n_pop):
             if scores[i] < best_eval:
                 best, best_eval = pop[i], scores[i]
-                print(f'>{gen}, new best f({decoded[i]}) = {scores[i]}')
+                print(f'>{gen}, new best f({pop[i]}) = {scores[i]}')
         # select parents
+
         selected = [selection(pop, scores) for _ in range(n_pop)]
         # create the next generation
         children = list()
+
+        if gen % 20:
+            r_mut = 0.5
+            if gen % 50:
+                r_mut = 1.0
+        else:
+            r_mut = r_mut0
+
+        # Вас не слышно
+
         for i in range(0, n_pop, 2):
             # get selected parents in pairs
             p1, p2 = selected[i], selected[i+1]
@@ -101,7 +120,15 @@ def genetic_algorithm(objective, bounds, n_bits, n_iter, n_pop, r_cross, r_mut):
                 # store for next generation
                 children.append(c)
         # replace population
-        pop = children
+        pop = best_parents + children
+
+        if gen == n_iter - 1:
+            best_eval_2 = 50
+            for i in range(n_pop):
+                if scores[i] < best_eval_2:
+                    best, best_eval_2 = pop[i], scores[i]
+                    print(f'LAST-GEN>{gen}, new best f({pop[i]}) = {scores[i]}')
+
     return [best, best_eval]
 
 
@@ -110,7 +137,7 @@ def main():
     bounds = [[-5.0, 5.0], [-5.0, 5.0]]
 
     # define the total iterations
-    n_iter = 100
+    n_iter = 500
 
     # bits per variable
     n_bits = 16
